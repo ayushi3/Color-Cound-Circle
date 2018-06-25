99b37d0d5998f03e8fd3feaeb6da9b95e12a864b
Ayushi Dhingra <ayushidhingra639@gmail.com>
2018-06-25 19:23:55 +0530

    Add files via upload

---
+++ circles.js
@@ -0,0 +1,20 @@
+var circles=[];
+
+		function onKeyDown(event) 
+		{
+			var maxPoint= new Point(view.size.width, view.size.height);
+			var randomPoint= Point.random();
+			var point=maxPoint*randomPoint;
+			var newCircle= new Path.Circle(point,);
+			newCircle.fillColor="orange";
+			circles.push(newCircle); 
+		}	
+
+		function onFrame(event)
+		{ 
+			for(var i =0; i< circles.length; i++)
+			{
+				circles[i].fillColor.hue+=1;
+				circles[i].scale(.9);
+			}
+		}
